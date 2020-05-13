b20015ad0d656a992dcf16ed9da989dd1a0abefa
Joao P Dubas <joao.dubas@gmail.com>
2020-05-13 07:39:12 -0300

    wip(handler) working on request handler.
    1. Add dummy methos to represent transformation steps
    2. Add code usage example

---
+++ lib/servy/handler.ex
@@ -0,0 +1,40 @@
+defmodule Servy.Handler do
+  def handle(request) do
+    request
+    |> parse()
+    |> route()
+    |> format_response()
+  end
+
+  def parse(_request) do
+    # TODO: parse the request into a map
+    %{method: "GET", path: "/wildthinks", body: ""}
+  end
+
+  def route(_conv) do
+    # TODO: create a new map that also has the response body
+    %{method: "GET", path: "/wildthinks", body: "Bears, Lions, Tigers"}
+  end
+
+  def format_response(_conv) do
+    # TODO: use values in the map to create a HTTP response string
+    """
+    HTTP/1.1 200 OK
+    Content-Type: text/html
+    Content-Length: 20
+
+    Bears, Lions, Tigers
+    """
+  end
+end
+
+request = """
+GET /wildthings HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.1
+Accept: */*
+
+"""
+
+response = Servy.Handler.handle(request)
+IO.puts response
