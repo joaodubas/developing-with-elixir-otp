684d46cd9a1a007466c78c8e9e77c40538270a40
Joao P Dubas <joao.dubas@gmail.com>
2020-05-14 22:01:34 -0300

    feat(handler) add response status code and reason

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -13,31 +13,42 @@ defmodule Servy.Handler do
       |> String.split("\n")
       |> List.first()
       |> String.split(" ")
-    %{method: method, path: path, body: ""}
+    %{method: method, path: path, body: "", status: nil}
   end
 
   def log(conv), do: IO.inspect conv
 
   def route(%{method: "GET", path: "/wildthings"} = conv) do
-    %{conv | body: "Bears, Lions, Tigers"}
+    %{conv | body: "Bears, Lions, Tigers", status: 200}
   end
 
   def route(%{method: "GET", path: "/bears"} = conv) do
-    %{conv | body: "Teddy, Smokey, Paddington"}
+    %{conv | body: "Teddy, Smokey, Paddington", status: 200}
   end
 
   def route(%{method: _, path: path} = conv) do
-    %{conv | body: "No #{path} in here!"}
+    %{conv | body: "No #{path} in here!", status: 404}
   end
 
   def format_response(conv) do
     """
-    HTTP/1.1 200 OK
+    HTTP/1.1 #{conv.status} #{status_reason(conv.status)}
     Content-Type: text/html
-    Content-Length: #{byte_size(conv[:body])}
+    Content-Length: #{byte_size(conv.body)}
 
-    #{conv[:body]}
+    #{conv.body}
     """
+  end
+
+  defp status_reason(code) do
+    %{
+      200 => "OK",
+      201 => "Created",
+      401 => "Unauthorized",
+      403 => "Forbidden",
+      404 => "Not Found",
+      500 => "Internal Server Error"
+    }[code]
   end
 end
 
