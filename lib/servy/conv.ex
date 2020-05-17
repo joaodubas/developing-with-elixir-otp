b89ebf65cb2a72a34f05b06c504091035523ea48
Joao P Dubas <joao.dubas@gmail.com>
2020-05-17 18:58:50 -0300

    feat(*) represent conv with a struct

---
+++ lib/servy/conv.ex
@@ -0,0 +1,20 @@
+defmodule Servy.Conv do
+  defstruct method: "", path: "", body: "", status: 0
+
+  def full_status(conv) do
+    "#{conv.status} #{status_reason(conv.status)}"
+  end
+
+  def body_size(%{body: body}), do: byte_size(body)
+
+  defp status_reason(code) do
+    %{
+      200 => "OK",
+      201 => "Created",
+      401 => "Unauthorized",
+      403 => "Forbidden",
+      404 => "Not Found",
+      500 => "Internal Server Error"
+    }[code]
+  end
+end
