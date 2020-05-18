eea0125adf745f636254bd34ce07d15d2dbdb807
Joao P Dubas <joao.dubas@gmail.com>
2020-05-18 09:12:46 -0300

    feat(parser) head body params from request

--- lib/servy/parser.ex
+++ lib/servy/parser.ex
@@ -3,11 +3,16 @@ defmodule Servy.Parser do
   alias Servy.Conv
 
   def parse(request) do
-    [method, path, _] =
-      request
-      |> String.split("\n")
-      |> List.first()
-      |> String.split(" ")
-    %Conv{method: method, path: path, body: "", status: nil}
+    [request_line, message_body] = String.split(request, "\n\n")
+    [general_header | _headers_string] = String.split(request_line, "\n")
+    [method, path, _protocol] = String.split(general_header, " ")
+    params = parse_body(message_body)
+    %Conv{method: method, path: path, params: params, body: "", status: nil}
+  end
+
+  def parse_body(message) do
+    message
+    |> String.trim()
+    |> URI.decode_query()
   end
 end
