a3abb7acbfdbd02a6d210e22ec04c0124c7e9504
Joao P Dubas <joao.dubas@gmail.com>
2020-05-20 20:29:07 -0300

    feat(parser) read/set headers map in conv

--- lib/servy/parser.ex
+++ lib/servy/parser.ex
@@ -4,11 +4,19 @@ defmodule Servy.Parser do
 
   def parse(request) do
     [request_line, message_body] = String.split(request, "\n\n")
-    [general_header | _headers_string] = String.split(request_line, "\n")
+    [general_header | headers_string] = String.split(request_line, "\n")
     [method, path, _protocol] = String.split(general_header, " ")
+    headers = parse_headers(headers_string, %{})
     params = parse_body(message_body)
-    %Conv{method: method, path: path, params: params, body: "", status: nil}
+    %Conv{method: method, path: path, params: params, headers: headers, body: "", status: nil}
   end
+
+  def parse_headers([head | tail], headers) do
+    [key, value] = String.split(head, ": ")
+    parse_headers(tail, Map.put(headers, key, value))
+  end
+
+  def parse_headers([], headers), do: headers
 
   def parse_body(message) do
     message
